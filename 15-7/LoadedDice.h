4a7a2d8b7db49b41ff96531b08b55b01e5065735
koko0828 <huangkoyu0828@gmail.com>
2024-06-16 21:55:59 +0800

    feat: add main.cpp

---
+++ 15-7/LoadedDice.h
@@ -0,0 +1,14 @@
+#ifndef LOADEDDICE_H
+#define LOADEDDICE_H
+#include"Dice.h"
+#include<iostream>
+using namespace std;
+
+class LoadedDice :public Dice			//dervied from Dice
+{
+public:
+	LoadedDice();						//default constructor
+	LoadedDice(int numSides);			//constructor
+	int rollDice()const override;		//override virtual function in base class
+};
+#endif
